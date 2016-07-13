1ca6e9d2ad60fb8c437e866e2629292d62f1bd07
Simon Finney <simonjfinney@gmail.com>
2016-07-13 09:33:16 +0100

    Adding right-to-left conversion

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -3,10 +3,12 @@
 
 'use strict';
 
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync');
+const rename = require('gulp-rename');
+const rtlcss = require('gulp-rtlcss');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 
 module.exports = (gulp, config) =>
   gulp.task('sass', () =>
@@ -18,5 +20,8 @@ module.exports = (gulp, config) =>
       .pipe(autoprefixer(config.autoprefixer))
       .pipe(sourcemaps.write())
       .pipe(gulp.dest(config.paths.basePath))
-      .pipe(browserSync.stream())
+      .pipe(rtlcss())
+      .pipe(rename({ basename: 'rtl' }))
+      .pipe(gulp.dest(config.paths.basePath))
+      .pipe(browserSync.stream());
   );
