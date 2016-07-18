d38712a6a8fe4396accb294e476c6a054f64c25f
Simon Finney <simonjfinney@gmail.com>
2016-07-18 18:57:57 +0100

    Adding styles for headings

--- app/wp-content/themes/school-self-evaluation/template-parts/content.php
+++ app/wp-content/themes/school-self-evaluation/template-parts/content.php
@@ -13,9 +13,9 @@
 	<header class="entry-header">
 		<?php
 			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
+				the_title( '<h2>', '</h2>' );
 			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+				the_title( '<h2><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 			}
 
 		if ( 'post' === get_post_type() ) : ?>
