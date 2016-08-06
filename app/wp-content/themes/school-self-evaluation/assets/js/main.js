264cb199225182e54ed8ee837d71e7da15bfdf80
Simon Finney <simonjfinney@gmail.com>
2016-08-06 16:11:55 +0100

    Add navigation focus logic

--- app/wp-content/themes/school-self-evaluation/assets/js/main.js
+++ app/wp-content/themes/school-self-evaluation/assets/js/main.js
@@ -8,9 +8,23 @@ import {
 
 
 const app = getElement('[data-app]');
-const styleButtons = getElements('[data-class]');
+const styleButtons = getElements('[data-class]', app);
+const subnavigationLinks = getElements('.sub-menu a', app);
 
 let currentStyleClass = null;
+
+
+function toggleSubnavigationFocus(event) {
+  const isFocusEvent = (event.type === 'focus');
+
+  const subnavigationMenu = event.target
+    .parentNode
+    .parentNode;
+
+  isFocusEvent ?
+    subnavigationMenu.setAttribute('data-active', '') :
+    subnavigationMenu.removeAttribute('data-active');
+}
 
 
 function toggleStyles(event) {
@@ -31,6 +45,12 @@ function toggleStyles(event) {
 
 function init() {
   forEachElement(styleButtons, styleButton => styleButton.addEventListener('click', toggleStyles));
+
+  // Handles tab navigation through the subnavigation
+  forEachElement(subnavigationLinks, subnavigationLink => {
+    subnavigationLink.addEventListener('focus', toggleSubnavigationFocus);
+    subnavigationLink.addEventListener('blur', toggleSubnavigationFocus);
+  });
 }
 
 
