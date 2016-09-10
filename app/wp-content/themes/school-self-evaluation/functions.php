25080e2d3d5f52305297eb4bf98ea037c32f05f4
Simon Finney <simonjfinney@gmail.com>
2016-09-10 13:14:20 +0100

    Adding function for custom editor styles

--- app/wp-content/themes/school-self-evaluation/functions.php
+++ app/wp-content/themes/school-self-evaluation/functions.php
@@ -129,6 +129,31 @@ function school_self_evaluation_scripts() {
 }
 
 
+function school_self_evaluation_insert_formats($init_array) {
+	$style_formats = array(
+		array(
+			'title' => 'Section',
+			'block' => 'section',
+			'classes' => 'section',
+      'wrapper' => true,
+		),
+	);
+
+	$init_array['style_formats'] = json_encode($style_formats);
+	return $init_array;
+}
+
+add_filter( 'tiny_mce_before_init', 'school_self_evaluation_insert_formats' );
+
+
+function school_self_evaluation_mce_buttons($buttons) {
+  array_unshift($buttons, 'styleselect');
+  return $buttons;
+}
+
+
+add_filter('mce_buttons_2', 'school_self_evaluation_mce_buttons');
+
 add_action( 'wp_enqueue_scripts', 'school_self_evaluation_scripts' );
 
 add_filter('next_posts_link_attributes', 'school_self_evaluation_posts_link_attributes');
