b97140cebc3673a4e7ece35356fe476f80b6aa20
Simon Finney <simonjfinney@gmail.com>
2016-09-11 14:04:20 +0100

    Adding editor plugin for custom sections

--- app/wp-content/themes/school-self-evaluation/functions.php
+++ app/wp-content/themes/school-self-evaluation/functions.php
@@ -129,26 +129,16 @@ function school_self_evaluation_scripts() {
 }
 
 
-function school_self_evaluation_insert_formats($init_array) {
-	$style_formats = array(
-		array(
-			'title' => 'Section',
-			'block' => 'section',
-			'classes' => 'section',
-      'wrapper' => true,
-		),
-	);
-
-	$init_array['style_formats'] = json_encode($style_formats);
-	return $init_array;
+function school_self_evaluation_mce_buttons($buttons) {
+  array_unshift($buttons, 'section');
+  return $buttons;
 }
 
-add_filter( 'tiny_mce_before_init', 'school_self_evaluation_insert_formats' );
+add_filter('mce_external_plugins', 'school_self_evaluation_register_tinymce_javascript');
 
-
-function school_self_evaluation_mce_buttons($buttons) {
-  array_unshift($buttons, 'styleselect');
-  return $buttons;
+function school_self_evaluation_register_tinymce_javascript($plugin_array) {
+  $plugin_array['SchoolSelfEvaluation'] = get_template_directory_uri() . '/js/tinymce-plugin.js';
+  return $plugin_array;
 }
 
 
