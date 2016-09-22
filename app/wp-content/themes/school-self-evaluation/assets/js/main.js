86fccdfd0613baf3b4bb330306f341a1464a33f5
Simon Finney <simonjfinney@gmail.com>
2016-09-22 20:55:40 +0100

    Removing cached commits

---
+++ app/wp-content/themes/school-self-evaluation/assets/js/main.js
@@ -0,0 +1,64 @@
+// TODO: Comments
+
+import {
+  forEachElement,
+  getElement,
+  getElements,
+} from './util';
+
+
+const app = getElement('[data-app]');
+const styleButtons = getElements('[data-class]', app);
+const subnavigationLinks = getElements('.sub-menu a', app);
+
+let currentStyleClass;
+
+
+function toggleSubnavigationFocus(event) {
+  const isFocusEvent = (event.type === 'focus');
+
+  const subnavigationMenu = event.target
+    .parentNode
+    .parentNode;
+
+  isFocusEvent ?
+    subnavigationMenu.setAttribute('data-active', '') :
+    subnavigationMenu.removeAttribute('data-active');
+}
+
+
+function toggleStyles(event) {
+  const styleButton = event.target;
+  const styleClass = styleButton.getAttribute('data-class');
+
+  if (currentStyleClass) {
+    app.classList.remove(currentStyleClass);
+  }
+
+  if (styleClass) {
+    app.classList.add(styleClass);
+  }
+
+  currentStyleClass = styleClass;
+  localStorage.setItem('class', currentStyleClass);
+}
+
+
+function init() {
+  currentStyleClass = localStorage.getItem('class');
+
+  if (currentStyleClass) {
+    app.classList.add(currentStyleClass);
+  }
+
+  forEachElement(styleButtons, styleButton => styleButton.addEventListener('click', toggleStyles));
+
+  // Handles tabbing through the subnavigation
+  forEachElement(subnavigationLinks, subnavigationLink => {
+    subnavigationLink.addEventListener('focus', toggleSubnavigationFocus);
+    subnavigationLink.addEventListener('blur', toggleSubnavigationFocus);
+  });
+}
+
+
+init();
---
+++ app/wp-content/themes/school-self-evaluation/assets/templates/404.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found).
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package School_Self-Evaluation
+ */
+
+ get_header();
+ ?>
+
+ <section class="content__article">
+   <h2><?php esc_html_e('Oops!', 'school-self-evaluation'); ?></h2>
+   <p><?php esc_html_e('It looks like that content can&rsquo;t be found in this location... Maybe try one of the links below or a search above?', 'school-self-evaluation'); ?></p>
+
+   <?php
+   the_widget('WP_Widget_Recent_Posts', 'title=Latest News', 'before_title=<h3>&after_title=</h3>');
+   ?>
+ </section>
+
+ <?php
+
+ get_sidebar();
+ get_footer();
+ ?>
