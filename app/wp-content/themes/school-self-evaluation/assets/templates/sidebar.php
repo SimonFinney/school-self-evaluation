972c7b67ac309275a0ff6468740558ac3210dedb
Simon Finney <SIMONFIN@ie.ibm.com>
2021-01-09 17:44:15 +0000

    fix(sidebar): add `dnt` attribute

--- app/wp-content/themes/school-self-evaluation/assets/templates/sidebar.php
+++ app/wp-content/themes/school-self-evaluation/assets/templates/sidebar.php
@@ -12,7 +12,7 @@ if (is_front_page() || is_page('contact')) { ?>
 
 <aside class="aside" role="complementary">
   <h3>Latest Tweets</h3>
-  <a class="twitter-timeline" height="480" href="https://twitter.com/SSEinspectorate" data-chrome="noborders nofooter noheader transparent" data-link-color="#326598">Tweets by SSEinspectorate</a>
+  <a class="twitter-timeline" height="480" href="https://twitter.com/SSEinspectorate" data-chrome="noborders nofooter noheader transparent" data-dnt="true" data-link-color="#326598">Tweets by SSEinspectorate</a>
   <?php dynamic_sidebar('sidebar-1'); ?>
 </aside>
 
